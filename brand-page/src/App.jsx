b6fd68fe120a8b4032ef3f8b35b2ab5006a273c1
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-06-11 19:22:33 +0500

    navigation added

--- brand-page/src/App.jsx
+++ brand-page/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
 }
 
 export default App;
