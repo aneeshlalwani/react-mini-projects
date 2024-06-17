b57948a61640b8be89e38ee8ca58a4a5154ccee1
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-06-17 20:44:44 +0500

    added contact header

--- contact-us-page/src/App.jsx
+++ contact-us-page/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import ContactHeader from "./components/ContactHeader";
 import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <>
       <NavigationBar />
+      <ContactHeader />
     </>
   );
 }
